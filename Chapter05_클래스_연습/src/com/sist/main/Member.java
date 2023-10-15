00181e2a2e863fd2af2ced89bcd6b56eb400f0a1
DevKim <ksssk96@gmail.com>
2023-10-15 23:18:18 +0900

    클래스 변수,구조 연습

---
+++ Chapter05_클래스_연습/src/com/sist/main/Member.java
@@ -0,0 +1,9 @@
+package com.sist.main;
+
+public class Member {
+	public int mno;
+	public  int dno;
+	public  String name;
+	public  String loc;
+	public  String dept;
+}
---
+++ Chapter05_클래스_연습/src/com/sist/main/MemberMain.java
@@ -0,0 +1,11 @@
+package com.sist.main;
+
+public class MemberMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Member mm=new Member();
+		
+	}
+
+}
