a8ee7725e28b631319c6e4b186ed569e3d342ae0
DevKim <ksssk96@gmail.com>
2023-10-21 12:13:45 +0900

    예외처리 마무리 + 네트워크프로젝트

--- JavaFinalProject/src/com/sist/client/ClientMain.java
+++ JavaFinalProject/src/com/sist/client/ClientMain.java
@@ -3,32 +3,31 @@ package com.sist.client;
 import javax.swing.*;
 import java.awt.event.*;
 /*
- *  Container : 윈도우창
- *    => JFrame : 닫기 ,확대 , 축소
- *    => JDialog : 
- *       = Modal
- *       = Modaless
- *    => JWindow : title bar가 없다
- *    => JPanel : 단독으로 실행이 안된다
- *       ------> Button,TextField 묶어서 사용
- *    
- *    Component : => 단독으로 실행할 수 없다
+ *   Container: 윈도우창
+ *     => JFrame: 닫기, 확대, 축소
+ *     => JDialog
+ *         = Modal
+ *         = Modaless
+ *     => JWindow: title바가 없다
+ *     => JPanel: 단독으로 실행이 안됨
+ *        ------> Button, TextField 묶어서 사용
+ *     => JTable, JTree, 
+ *   Component: => 단독으로 실행 할 수 없음
  *     버튼
- *      = 일반 버튼 : JButton
- *      = 토글 버튼 : JRadioButton , JCheckbox
- *      = 메뉴 : JMenuItem
+ *       = 일반 버튼: JButton
+ *       = 토글 버튼: JRadioButton, JCheckBox
+ *       = 메뉴: JMenuItem
  *     입력
- *      = 한 줄 입력 : JTextField
- *      = 여러줄 입력 : JTExtArea
- *      = 비밀번호 입력 : JPasswordField
+ *       = 한 줄 입력: JTextField
+ *       = 여러 줄 입력: JTextArea
+ *       = 비밀번호 입력: JPasswordField
  *     기타
- *      =문자 출력 , 이미지 출력 , JLabel
- *      = JTable : 목록 출력 , (JList)
- *      = JTabbedPane , JEditPane , JTextPane
- *
+ *       = 문자 출력, 이미지 출력: JLabel
+ *       = JTable: 목록출력,(JList)
+ *       = JTabbedPane, JEditPane, JTextPane
  */
-
 public class ClientMain implements ActionListener{
+	// 생성자 => 오버로딩
 	private JFrame fr=new JFrame("네트워크 미니 프로젝트");
 	Login login=new Login();
 	/*private JButton b1=new JButton("홍");
@@ -44,7 +43,7 @@ public class ClientMain implements ActionListener{
 	JTabbedPane pan=new JTabbedPane();
 	public ClientMain()
 	{
-		pan.setTabPlacement(JTabbedPane.LEFT);
+		pan.setTabPlacement(JTabbedPane.TOP);
 		pan.addTab("홈", new JPanel());
 		pan.addTab("맛집", new JPanel());
 		pan.addTab("레시피", new JPanel());
@@ -77,19 +76,15 @@ public class ClientMain implements ActionListener{
 			UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
 		}catch(Exception ex) {}
 		new ClientMain();
-		
-			
-		}
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if(e.getSource()==login.b1)
 		{
-
 			login.fr.setVisible(false);
 			fr.setVisible(true);
-	    }
-		
+		}
 	}
 
 }
